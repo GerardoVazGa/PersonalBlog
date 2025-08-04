54fc4a4e391a9979ea39c21414577b5aa3bd328b
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-08-03 20:38:03 -0600

    Added blog header navbar, footer and principal view home

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
