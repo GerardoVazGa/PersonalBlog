746c4d74388e1c1b2cca7395b215e244807d5e77
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-08-18 14:29:12 -0600

    Adding new dropdown menu and active option desing

--- public/javascript/dinamicNav.js
+++ public/javascript/dinamicNav.js
@@ -1,6 +1,29 @@
 const hambutton = document.querySelector('.hambutton')
+const dropdownBtn = document.querySelector('.dropbtn')
+const navLinks = document.querySelectorAll('.nav-links a')
 
 hambutton.addEventListener("click", (e) => {
     e.preventDefault()
     hambutton.classList.toggle('open')
 })
+
+dropdownBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const dropdownContent = document.querySelector('.dropdown-content')
+
+    dropdownContent.classList.toggle('active')
+})
+
+navLinks.forEach(navLink => {
+    navLink.addEventListener('click', e => {
+        navLinks.forEach(link => link.classList.remove('active'))
+        navLink.classList.add('active')
+    })
+})
+
+document.addEventListener("click", (e) => {
+  const dropdownContent = document.querySelector('.dropdown-content')
+  if (!dropdownContent.contains(e.target) && e.target !== dropdownBtn) {
+    dropdownContent.classList.remove("active");
+  }
+});
