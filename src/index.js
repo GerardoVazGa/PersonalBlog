43a4caa885b3763a6b3240c1636b89b0bc926677
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-08-18 22:48:29 -0600

    Adding endpoint categories and its view

--- src/index.js
+++ src/index.js
@@ -11,11 +11,19 @@ const app = express()
 const port = process.env.PORT || 3000
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+const categories = ['Technology', 'Animation', 'Programming', 'Cibersegurity']
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.json())
 
 app.use(express.static('public'))
+
+const useCategories = (req, res, next) => {
+    res.locals.categories = categories
+    next()
+}
+
+app.use(useCategories)
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -26,6 +34,11 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about.ejs', {current: 'about'})
+})
+
+app.get('/category/:category', (req, res) => {
+    const category = req.params.category.toLowerCase()
+    res.render('category', {current: category})
 })
 
 
