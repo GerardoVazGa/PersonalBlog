e4eabfc8bb899581efad05a31336082059d83c6a
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-09-04 20:57:47 -0600

    Adding util sanatizer for the dirty content

---
+++ src/utils/sanatizer.js
@@ -0,0 +1,17 @@
+
+import DOMPurify from "dompurify"
+import {JSDOM} from "jsdom"
+
+export const sanatizer = (dirtyContent) => {
+    const {window}  = new JSDOM("")
+    const purify = DOMPurify(window)
+
+    const cleanContent = purify.sanitize(dirtyContent, {
+        ALLOWED_TAGS: ['p', 'strong', 'em', 'u', 'ol', 'ul', 'li', 'img', 'h2', 'h3', 'h4', 'h5', 'h6', 'br', 'a', 'code', 'pre', 'div', 'blockquote'],
+        ALLOWED_ATTR: ['href', 'src', 'alt', 'title', 'height', 'width', 'style', 'class'],
+        FORBID_ATTR: ['onclick', 'onerror']
+    })
+
+    return cleanContent
+}
+
