7f3ba7f90f34ff0cb94f1e74d1bd12d69bff0e54
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-09-30 19:07:02 -0600

    Creating a model get posts by its category

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -11,3 +11,15 @@ export const getCategoryId = async (name) => {
     const [rows] =await pool.query(query, [name])
     return rows.length > 0 ? rows[0].id : null
 }
+
+export const getPostsByCategory = async (categoryName) => {
+    const query = `
+        SELECT p.*, c.name as category_name
+        FROM posts p
+        INNER JOIN categories c ON p.category_id = c.id
+        WHERE c.name = ?    
+    `
+    const [result] = await pool.query(query, [categoryName])
+
+    return result
+}
