dba7a33ed08548dce9664b807f93f68c485ea73f
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-10-08 21:00:40 -0600

    Adding new storage to session

--- src/configs/session_config.js
+++ src/configs/session_config.js
@@ -1,7 +1,12 @@
 import {SESSION_SECRET} from "./env.js"
+import MySQLStore from "express-mysql-session"
+import pool from "../db/db.js"
+
+const sessionStore = new MySQLStore({}, pool)
 
 export const sessionConfig = {
-    secret: SESSION_SECRET, 
+    secret: SESSION_SECRET,
+    store: sessionStore,
     resave: false,
     saveUninitialized: true
 }
