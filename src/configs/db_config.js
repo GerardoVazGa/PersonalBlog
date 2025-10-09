d8603e71d946224abdd6150ef27df2a90088b3c5
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-10-08 20:04:02 -0600

    Adding the DB_Port in the database configuration

--- src/configs/db_config.js
+++ src/configs/db_config.js
@@ -1,8 +1,9 @@
-import {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME} from "./env.js"
+import {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT} from "./env.js"
 
 export const dbConfig = {
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    database: DB_NAME
+    database: DB_NAME,
+    port: DB_PORT
 }
--- src/configs/env.js
+++ src/configs/env.js
@@ -8,6 +8,7 @@ export const DB_HOST = process.env.DB_HOST || "localhost"
 export const DB_USER = process.env.DB_USER || "root"
 export const DB_PASSWORD = process.env.DB_PASSWORD || ""
 export const DB_NAME = process.env.DB_NAME || ""
+export const DB_PORT = process.env.DB_PORT || 3306
 
 export const ADMIN_PASS = process.env.ADMIN_PASS
 export const SESSION_SECRET = process.env.SESSION_SECRET
