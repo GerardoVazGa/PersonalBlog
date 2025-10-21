2882631adb36da8fb72d950384e48cecbaab7905
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-10-20 18:03:36 -0600

    Add new mobile nav bar on the blog

--- public/javascript/dinamicNav.js
+++ public/javascript/dinamicNav.js
@@ -1,16 +1,29 @@
 const hambutton = document.querySelector('.hambutton')
 const dropdownBtn = document.querySelector('.dropbtn')
 const dropdownContent = document.querySelector('.dropdown-content')
+const mobileNav = document.querySelector('.mobile-nav')
+const categoryBtn = document.querySelector('.nav-item.category-btn-container')
+const categoriesDropdown = document.querySelector('.category-dropdown')
+const arrowIcon = document.querySelector('.fa-solid.fa-caret-down')
 
 hambutton.addEventListener("click", (e) => {
     e.preventDefault()
     hambutton.classList.toggle('open')
+    mobileNav.classList.toggle('active')
 })
 
 dropdownBtn.addEventListener("click", (e) => {
     e.preventDefault()
     dropdownContent.classList.toggle('open')
 })
+
+if(mobileNav) {
+  categoryBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    categoriesDropdown.classList.toggle("open");
+    arrowIcon.classList.toggle("rotate")
+  });
+}
 
 document.addEventListener("click", (e) => {
   if (!dropdownContent.contains(e.target) && e.target !== dropdownBtn) {
