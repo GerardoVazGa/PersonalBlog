00421c8e53751c70831257dcd128ac0c45fc0e90
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2025-12-22 21:09:14 -0600

    Adding the login button on responsive menu and a light change in auth function

--- public/javascript/authModal.js
+++ public/javascript/authModal.js
@@ -1,80 +1,87 @@
-import {showModal} from "./modal.js"
+import { showModal } from "./modal.js";
 
 //Login modal
-const loginBtn = document.querySelector('#loginBtn')
-const logoutBtn = document.querySelector('#logoutBtn')
+const loginBtns = document.querySelectorAll(".loginBtn");
+const logoutBtns = document.querySelectorAll(".logoutBtn");
 
-if (loginBtn) {
-    loginBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        showModal({
-            title: "Admin Login",
-            size: "small",
-            content: (content) => {
-                const form = document.createElement("form")
-                form.id = "passwordForm"
-                form.classList.add("form-inputs")
+if (loginBtns.length > 0) {
+    loginBtns.forEach((loginBtn) => {
+        loginBtn.addEventListener("click", (e) => {
+            e.preventDefault()
+            showModal({
+                title: "Admin Login",
+                size: "small",
+                content: (content) => {
+                    const form = document.createElement("form");
+                    form.id = "passwordForm";
+                    form.classList.add("form-inputs");
 
-                const passwordInput = document.createElement("input")
-                passwordInput.id = "passwordInput"
-                passwordInput.type = "password"
-                passwordInput.placeholder = "Enter your password"
+                    const passwordInput = document.createElement("input");
+                    passwordInput.id = "passwordInput";
+                    passwordInput.type = "password";
+                    passwordInput.placeholder = "Enter your password";
 
-                form.appendChild(passwordInput)
-                content.appendChild(form)
-            },
-            onAction: async () => {
-                const password = document.getElementById("passwordInput").value
-                try {
-                    const response = await fetch('/login', {
-                        method: "POST",
-                        headers: {
-                            "content-type": "application/json"
-                        },
-                        body: JSON.stringify({password})
-                    })
+                    form.appendChild(passwordInput);
+                    content.appendChild(form);
+                },
+                onAction: async () => {
+                    const password = document.getElementById("passwordInput").value;
+                    try {
+                        const response = await fetch("/login", {
+                            method: "POST",
+                            headers: {
+                                "content-type": "application/json",
+                            },
+                            body: JSON.stringify({ password }),
+                        });
 
-                    const data = await response.json()
+                        const data = await response.json();
 
-                    if(data.success) {
-                        setTimeout(() => {
-                            window.location.reload()
-                        }, 1000)
-                    } else {
-                        console.error('Error al iniciar sesión:', data.message)
+                        if (data.success) {
+                            setTimeout(() => {
+                                window.location.reload();
+                            }, 1000);
+                        } else {
+                            console.error("Error al iniciar sesión:", data.message);
+                        }
+                    } catch (error) {
+                        console.error("Error al iniciar sesión:", error.message);
                     }
-                } catch (error) {
-                    console.error('Error al iniciar sesión:', error.message)
-                }
-            },
-            buttonText: "Login"
-        })
+                },
+                buttonText: "Login",
+            });
+        });
     });
 }
 
-if(logoutBtn) {
-    logoutBtn.addEventListener("click", logout)
+if (logoutBtns.length > 0) {
+    logoutBtns.forEach((logoutBtn) => {
+        logoutBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            logout();
+        });
+    });
 }
 
 async function logout() {
-    try{
-        const response = await fetch('/logout', {
-            method: 'POST',
+    try {
+        const response = await fetch("/logout", {
+            method: "POST",
             headers: {
-                "content-type": "application/json"
-            }
-        })
+                "content-type": "application/json",
+            },
+        });
 
-        const data = await response.json()
+        const data = await response.json();
 
-        if(data.success) {
+        if (data.success) {
             setTimeout(() => {
-                window.location.reload()
-            }, 1000)
+                window.location.reload();
+            }, 1000);
         } else {
-            console.error('Error al cerrar sesión:', data.message)
+            console.error("Error al cerrar sesión:", data.message);
         }
-    }catch(error){
-        console.error('error al cerrar sesion:', error.message)
+    } catch (error) {
+        console.error("error al cerrar sesion:", error.message);
     }
 }
