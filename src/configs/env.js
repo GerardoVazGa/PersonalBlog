02a142310f3b715fda97ce4842a351136562fd7c
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-02-25 12:25:53 -0600

    config(env): update database defaults for PostgreSQL

--- src/configs/env.js
+++ src/configs/env.js
@@ -5,10 +5,10 @@ dotenv.config()
 export const PORT = process.env.PORT || 3000
 
 export const DB_HOST = process.env.DB_HOST || "localhost"
-export const DB_USER = process.env.DB_USER || "root"
+export const DB_USER = process.env.DB_USER || "postgres"
 export const DB_PASSWORD = process.env.DB_PASSWORD || ""
 export const DB_NAME = process.env.DB_NAME || ""
-export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_PORT = process.env.DB_PORT || 5432
 
 export const ADMIN_PASS = process.env.ADMIN_PASS
 export const SECRET_JWT_KEY = process.env.SECRET_JWT_KEY || "DEFAULT_SECRET"
