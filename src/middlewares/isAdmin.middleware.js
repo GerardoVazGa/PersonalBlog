2a9b7b3ea000e3b47db2298f5ad3514ced8fe015
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-02-25 12:43:46 -0600

    fix(middleware): correct session reference in isAdmin middleware

--- src/middlewares/isAdmin.middleware.js
+++ src/middlewares/isAdmin.middleware.js
@@ -1,6 +1,6 @@
 
 export function isAdmin(req, res, next) {
-    if(res.session?.isAdmin !== true) {
+    if(req.session?.isAdmin !== true) {
         return res.status(403).json({success: false, message: "Admin access denied"})
     }
 
