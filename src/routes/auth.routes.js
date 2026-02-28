74a1c8a96a45c8c1ed471f4376d94dd71aeaa138
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-02-27 20:12:21 -0600

    fix(routes): fix incorrect controller import in auth.routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import {loginAdmin, logoutAdmin} from "./controllers/auth.controller.js"
+import {loginAdmin, logoutAdmin} from "../controllers/auth.controller.js"
 
 const router = Router()
 
