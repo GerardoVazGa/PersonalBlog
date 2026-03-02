b07d23529588e04a6f4892bddf6dfa15b752a59f
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-03-02 15:33:02 -0600

    feat(routes): include new route /blog to get and view the all posts

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { getRecentPosts, getPost, getPostsByCategory } from "../controllers/posts.controller.js"
+import { getRecentPosts, getPost, getPostsByCategory, getAllPosts } from "../controllers/posts.controller.js"
 
 const router = Router()
 
 router.get('/', getRecentPosts)
+router.get('/blog', getAllPosts)
 router.get('/about', (req, res) => {
     res.render('about.ejs', {current: 'about'})
 })
