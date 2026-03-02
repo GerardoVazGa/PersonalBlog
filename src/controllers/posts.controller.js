68802c9002c3cadf6e39f77799fa817fb372e290
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-03-02 16:53:11 -0600

    refactor(posts-controller): add page param to use in the pagination support

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -2,8 +2,9 @@ import * as PostService from "../services/post.service.js"
 import { formatDate } from "../utils/formatDate.js"
 
 export const getAllPosts = async (req, res) => {
+    const page = req.params.page || 1
     try {
-        const {posts, meta} = await PostService.getAllPosts(req.query)
+        const {posts, meta} = await PostService.getAllPosts(page)
         res.json({success: true, posts, meta})
     } catch (error) {
         res.status(500).json({success: false, error: error.message})
@@ -78,9 +79,9 @@ export const deletePost = async (req, res) => {
 
 export const getPostsByCategory = async (req, res) => {
     const {category} = req.params
-
+    const page = req.params.page || 1
     try {
-        const {posts, meta} = await PostService.getPostsByCategory(category, req.query)
+        const {posts, meta} = await PostService.getPostsByCategory(category, page)
 
         if(posts) {
             return res.status(200).render('category.ejs', {current: category, posts, meta})
