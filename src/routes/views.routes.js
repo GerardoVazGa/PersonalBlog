db7bb27a56471a49f52ddbe0f0963e6a806aed5f
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-03-02 16:04:23 -0600

    refactor(routes): add pagination param to use pagination support in category

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -10,6 +10,7 @@ router.get('/about', (req, res) => {
     res.render('about.ejs', {current: 'about'})
 })
 router.get('/blog/category/:category', getPostsByCategory)
+router.get('/blog/category/:category/page/:page', getPostsByCategory)
 router.get('/post/:slug',  getPost)
 
 export default router
