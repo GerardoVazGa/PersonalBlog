15b40337228a664514efe25a04d9af9dd3349beb
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-03-03 16:07:43 -0600

    refacctor(views): include the scripts partial in each view

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,7 +4,7 @@ import {getPostById} from "../controllers/posts.controller.js"
 
 const router = Router()
 
-router.get('/posts/new', isAdmin, getViewCreatePost)
+// router.get('/posts/new', isAdmin, getViewCreatePost)
 router.get('/posts/:id/edit', isAdmin, getPostById)
 
 export default router
