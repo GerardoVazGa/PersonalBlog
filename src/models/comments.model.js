294e06867fb17df26f7c28f83d5c8368eb8897f7
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-03-24 11:44:09 -0600

    feat(models): add new comment on the database

--- src/models/comments.model.js
+++ src/models/comments.model.js
@@ -17,3 +17,20 @@ export async function getCommentsByPostId(postId) {
         throw error;
     }
 }
+
+export async function addComment(postId, content, authorName, parentCommentId = null) {
+    try {
+        const query = `
+            INSERT INTO comments (content, author_name, post_id, parent_comment_id)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *
+        `
+        const result = await pool.query(query, [content, authorName, postId, parentCommentId])
+
+        return result.rows[0]
+        
+    } catch (error) {
+        console.error("Error adding comment:", error);
+        throw error;
+    }
+}
