a916a81ce104c09187fd9cbc9a525afc2de877c9
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-03-25 12:14:13 -0600

    feat(routes): add routes to likes comments and delete comments

--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express"
-import { getComments, addComment } from "../controllers/comments.controller.js"
+import { getComments, addComment, likeComment, deleteComment } from "../controllers/comments.controller.js"
 
 const router = Router()
 
 router.get('/post/:postId/comments', getComments)
 router.post('/post/:postId/comments', addComment)
+
+router.patch('/comments/:commentId/like', likeComment)
+router.delete('/comments/:commentId', deleteComment)
 
 export default router
