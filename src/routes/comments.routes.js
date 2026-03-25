8357ee7c31b5cfbca23c3b0b0234ce19aefb27d9
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-03-25 11:00:07 -0600

    feat(routes): add route to insert new comments

--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { getComments } from "../controllers/comments.controller.js"
+import { getComments, addComment } from "../controllers/comments.controller.js"
 
 const router = Router()
 
 router.get('/post/:postId/comments', getComments)
+router.post('/post/:postId/comments', addComment)
 
 export default router
