253f13d43d4ba0c9935b983fb670c8688cab1e9b
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-03-25 10:56:21 -0600

    feat(services): add validations and condition to insert comments

--- src/services/comments.service.js
+++ src/services/comments.service.js
@@ -4,3 +4,26 @@ export const getComments = async (postId) => {
     const comments = await CommentsModel.getCommentsByPostId(postId)
     return comments
 }
+
+export const addComment = async (postId, content, authorName, parentCommnetId = null) => {
+    if(!postId || !content || !authorName){
+        throw new Error("Missing required fields");
+    }
+
+    const cleanContent = content.trim()
+
+    if(cleanContent.length === 0 || cleanContent.length > 1000) {
+        throw new Error("Content must be between 1 and 1000 characters")
+    }
+
+    const cleanAuthorName = authorName.trim()
+
+    if(cleanAuthorName.length === 0 || cleanAuthorName.length > 20) {
+        throw new Error("Author name must be between 1 and 20 characters")
+    }
+
+    const comment = await CommentsModel.addComment(postId, cleanContent, cleanAuthorName, parentCommnetId)
+
+    return comment
+
+}
