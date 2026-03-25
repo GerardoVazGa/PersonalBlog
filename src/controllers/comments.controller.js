60f8a95832cf5d52fc5a76756a33dad057d2624b
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-03-25 10:58:20 -0600

    feat(controllers): add controller to handle requests and response adding comments

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -10,3 +10,16 @@ export const getComments = async (req, res) => {
         res.status(500).json({ error: "Failed to fetch comments" });
     }
 }
+
+export const addComment = async (req, res) => {
+    const { postId } = req.params
+    const { content, authorName , parentCommentId = null } = req.body 
+
+    try {
+        const comment = await CommentsService.addComment(postId, content, authorName, parentCommentId)
+        res.status(201).json(comment)
+    } catch (error) {
+        console.error("Error adding comment:", error);
+        res.status(400).json({ error: "Failed to add comment" });
+    }
+}
