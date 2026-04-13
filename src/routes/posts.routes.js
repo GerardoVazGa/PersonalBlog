a09da58c88b3184057a756a0eb782c690965cf46
GerardoVazGa <gerardo.vaz.galindo@gmail.com>
2026-04-13 12:09:18 -0600

    feat(routes): search route to get specific posts

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import {getPostJson, addPost, editPost, deletePost} from "../controllers/posts.controller.js"
+import {getPostJson, addPost, editPost, deletePost, searchPosts} from "../controllers/posts.controller.js"
 import { isAdmin } from '../middlewares/isAdmin.middleware.js'
 import {uploadTemp} from '../configs/uploads_config.js'
 
 const router = Router()
 
+router.get('/search', searchPosts)
 router.get('/:id', getPostJson)
 router.post('/', uploadTemp.single('image'), isAdmin, addPost)
 router.put('/:id', uploadTemp.single('image'), isAdmin, editPost)
